cc947869bd70b8f63893ebc1034f1ca68abe3102
Alistair Johnson <alistairewj@gmail.com>
2011-09-16 08:06:03 +0100

    Updated xval_None (default xval) to create 50 random train/test indices using 30% test and 70% training.

--- xval_None.m
+++ xval_None.m
@@ -1,6 +1,26 @@
-function [ tr_data, t_data, idx ] = xval_None( tr_data, t_data, idx )
+function [ tr_idx, t_idx, D ] = xval_None( data_target )
 %XVAL_NONE No Cross Validation used.
+%   Default is 50 randomly assigned indices 70%/30% training/test.
 
+D=50;
+P=0.3;
+N=length(data_target);
+    
+%     random indices from 1:N
+[~,temp] = sort(rand(N,D),1);
+  
+% Temporary numerical indices
+tr_idx=false(N,D);
+t_idx=false(N,D);
+
+test1 = (temp(1:floor(N*P),:));
+train1=temp(floor(N*P)+1:end,:);
+
+%TODO: make this vectorized
+for k=1:D
+    tr_idx(train1(:,k),k)=true;
+    t_idx(test1(:,k),k)=true;
+end
 
 end
 
