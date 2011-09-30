49846f6243329636495ce5b0454c3289975d11ee
Alistair Johnson <alistairewj@gmail.com>
2011-09-30 20:23:00 +0100

    Vectorized a bit of new_generation.m
    Old time: 0.0132s
    New time: 0.0081s
    Speed-up: 1.629:1

--- new_generation.m
+++ new_generation.m
@@ -19,7 +19,7 @@ mutation_prop = .2 ; % TODO: Make this a parameter in options
 
 % sort parent generation by increasing or decreasing performance
 [BestPerfA  BestPerfAIdx] = sort(PerfA,sort_str);
-% Pass down the 10% best performers to the next generation 
+% Pass down the X% best performers to the next generation 
 elderly = parent( BestPerfAIdx(1:POP_elite) , : ) ; 
 % Select the 1st half of the population to undergo cross-over
 repro = parent( BestPerfAIdx(1:POP_xover) , : ) ;
@@ -30,21 +30,20 @@ repro = parent( BestPerfAIdx(1:POP_xover) , : ) ;
 
 %% Mutation
 [ children ] = feval(mutFcn, children, [mutation_prop, mutation_rate]);
-%[ children ] = feval(@mut_SP, children, [.2, mutation_rate]); % Modified by Louis 1st-Jul-11
 
 %% Make sure that we keep below MaxVar and that confounding factors are
 % still included
-if MaxVar~=0
-    for p=1:2*POP_xover
+if MaxVar~=0 % Ensure number of features < MaxVar
+    for p=1:2*POP_xover % TODO: Vectorize
         tbr = 1;
         while tbr~=0
             children(p,ConfFact) = 1; % Force confounding factors        
-            tbr = sum(children(p,:)) - MaxVar;
-            if tbr>0
+            tbr = sum(children(p,:)) - MaxVar; % tbr = # of excess features
+            if tbr>0 % Reduce # features
                 idx = find(children(p,:)==1);
                 idx_tbr = idx(1+floor(length(idx)*rand(1,1)));
                 children(p,idx_tbr) = 0 ;            
-            elseif tbr<0
+            elseif tbr<0 % Increase # features
                 idx = find(children(p,:)==0);
                 idx_tbr = idx(1+floor(length(idx)*rand(1,1)));
                 children(p,idx_tbr) = 1 ;
@@ -52,19 +51,18 @@ if MaxVar~=0
 
         end
     end
-else
+else % No maximum set for number of features
     % Force confounding factors     
     children(:,ConfFact)=true(size(children,1),length(ConfFact));
 end
 
 %% Replace twins by aliens
 twins = zeros(POP_xover,1);
+nonTwinIdx=true(2*POP_xover,1);
+% twinCmpFcn=@(children,i) any(sqrt(sum((children(i,:)-children(1:i-1,:)).^2))==0);
 for i = 1:2*POP_xover
-    for j = 1:(i-1)
-        if sqrt( sum( (children(i,:) - children(j,:)).^2 ) ) == 0
-            twins(i) = 1 ;
-        end
-    end
+    tmpValue=bsxfun(@minus,children(1:i-1,:),children(i,:));
+    twins(i)=any(sqrt(sum(tmpValue.^2))==0);
 end
 if sum(twins)>0
     options.PopulationSize=sum(twins); % Temporary change.
