72fbeff908016cc074db9ddd8b248a6816142ce8
Alistair Johnson <alistairewj@gmail.com>
2011-11-27 20:50:42 +0000

    Fixed SVM fitness function and added template function (incomplete).

--- demo1.m
+++ demo1.m
@@ -1,69 +1,29 @@
 % % demo1
 % % This demo shows the user how to perform feature selection with the GA
 % % toolbox using all default parameters.
-% loaddata;
-% load AlistairDATA_mv2.mat
+
+
+%== First, load the data
+load simulated_binary.mat
+
+%=== Open parallel processing if using
 matlabpool;
+
+%=== Next, instantiate the GA options
+% A full list of options is available in the help file
+opts=ga_opt_set('Parallelize',1,'CostFcn',@cost_AUROC,'OptDir',1,...
+    'ErrorIterations',20,'ErrorGradient',0.005,...
+    'MinimizeFeatures',false,'PlotFcn','plot_All',...
+    'PopulationSize', 48,'FitnessFcn','fit_LR',...
+    'Repetitions', 4,'MaxIterations',50,'Display','none');
+
+% Run the GA
 fprintf('Here we go! \n');
-% % First, load the data
-% load arrhythmia;
-% disp(Description)
+[ga_out, ga_options] =  AlgoGen(data,outcome,opts);
 
-% Next, instantiate the GA options
-% A full list of options is available in the help file
-opts=ga_opt_set('Parallelize',1,'CostFcn',@cost_AUROC,'OptDir',1,...
-    'ErrorIterations',20,'ErrorGradient',0.005,...
-    'MinimizeFeatures',true,...
-    'PopulationSize', 48,'FitnessFcn','fit_LR','NumActiveFeatures',[],...
-    'Repetitions', 4,'MaxIterations',50,'Display','none');
+%=== Save the output
+save('GA_output_1.mat','ga_out','ga_options');
 
-% Run the GA, ~8.5 hours per rep
-[ga_out1, ga_options1] =  AlgoGen(data,outcome,opts);
-save('GA_ALISTAIR_mv2_1.mat','ga_out1','ga_options1');
+
+%=== Close the parallel toolbox
 matlabpool close;
-matlabpool;
-% Next, instantiate the GA options
-% A full list of options is available in the help file
-opts=ga_opt_set('Parallelize',1,'CostFcn',@cost_AUROC,'OptDir',1,...
-    'ErrorIterations',20,'ErrorGradient',0.005,...
-    'MinimizeFeatures',false,...
-    'PopulationSize', 48,'FitnessFcn','fit_LR','NumActiveFeatures',[],...
-    'Repetitions', 4,'MaxIterations',50,'Display','none');
-
-% Run the GA, ~8 hours
-[ga_out2, ga_options2] =  AlgoGen(data,outcome,opts);
-save('GA_ALISTAIR_mv2_2.mat','ga_out2','ga_options2');
-
-matlabpool close;
-matlabpool;
-
-% Next, instantiate the GA options
-% A full list of options is available in the help file
-opts=ga_opt_set('Parallelize',1,'CostFcn',@cost_AUROC,'OptDir',1,...
-    'ErrorIterations',20,'ErrorGradient',0.005,...
-    'MinimizeFeatures',false,...
-    'PopulationSize', 48,'FitnessFcn','fit_LR','NumActiveFeatures',8,...
-    'Repetitions', 4,'MaxIterations',50,'Display','none');
-
-% Run the GA, ~8 hours
-[ga_out3, ga_options3] =  AlgoGen(data,outcome,opts);
-save('GA_ALISTAIR_mv2_3.mat','ga_out3','ga_options3');
-
-
-matlabpool close;
-matlabpool;
-% Next, instantiate the GA options
-% A full list of options is available in the help file
-opts=ga_opt_set('Parallelize',1,'CostFcn',@cost_AUROC,'OptDir',1,...
-    'ErrorIterations',20,'ErrorGradient',0.005,...
-    'MinimizeFeatures',true,...
-    'PopulationSize', 48,'FitnessFcn','fit_LR','NumActiveFeatures',8,...
-    'Repetitions', 4,'MaxIterations',50,'Display','none');
-
-% Run the GA, ~8.5 hours
-[ga_out1b, ga_options1b] =  AlgoGen(data,outcome,opts);
-save('GA_ALISTAIR_mv2_1b.mat','ga_out1b','ga_options1b');
-
-matlabpool close;
-
-% Can plot some interesting features
--- fit_SVM.m
+++ fit_SVM.m
@@ -1,28 +1,32 @@
 function [ train_pred, test_pred  ] = ...
-    fit_TEMPLATE(train_data,train_target,test_data,test_target)
-% Apply the TEMPLATE model to the data
-% [ train_pred, test_pred  ] = 
-%   fit_TEMPLATE(train_data,train_target,test_data,test_target)
-% Louis Mayaud, Oct. 4th 2011
+    fit_SVM(train_data,train_target,test_data,test_target)
+% FIT_SVM uses an SVM as the fitness function (wrapper)
 
-
-% train your model
-model = train_funtion(train_data,train_target) ;
+% train the model
+model = svmtrain(train_target, train_data, '-b 1');
 
 % Apply to your data
-[train_pred] = apply_model( train_data , model );
-[test_pred] = apply_model( test_data , model );
+[train_pred, train_acc, train_prob] = ...
+    svmpredict(train_target, train_data, model, '-b 1');
+[test_pred, test_acc, test_prob] = ...
+    svmpredict(test_target, test_data, model, '-b 1');
 
-
-    function model  = train_funtion(train_data,train_target)
-        % train your model here
-        
+%=== Make sure SVM doesn't output opposite predictions
+if ~isempty(train_prob)
+    if min(round(train_prob(:,1))==train_pred)==1
+        train_pred=train_prob(:,1);
+    else
+        train_pred=train_prob(:,2);
     end
+end
 
-    function prediction = apply_model( test_data , model )
-        % Apply your model here
-        
+if ~isempty(test_prob)
+    if min(round(test_prob(:,1))==test_pred)==1
+        test_pred=test_prob(:,1);
+    else
+        test_pred=test_prob(:,2);
     end
+end
 
 end
   
