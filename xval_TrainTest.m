ae27923a53cebd0c84635f422feb56744ec71248
Louis Mayaud <louis.mayaud@gmail.com>
2011-12-07 20:13:48 +0000

    Added new cost function BER (Balanced Error Rate) Added the same statistics (BER) Created the Train-Test cross validation technique Added exportation procedure to a file (support for xls and csv) Few edits here and there...

---
+++ xval_TrainTest.m
@@ -0,0 +1,14 @@
+function [ tr_idx, t_idx, D ] = xval_TrainTest( data_target, options )
+%XVAL_TrainTest : for 2 distinctives Training and test sets
+%   Default assigne indices 70%/30% training/test.
+
+D=1; % Number of fitness repetitions
+P=1/3; % Data split
+N=length(data_target);
+    
+tr_idx = false(N,1);
+tr_idx(1:floor(P*N)) = true ;
+t_idx = ~tr_idx;
+
+
+end
