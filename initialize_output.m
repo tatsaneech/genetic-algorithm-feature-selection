60770148b09da5a67fa2e86d0f7cbd42723d5ee0
Alistair Johnson <alistairewj@gmail.com>
2012-01-19 12:27:30 +0000

    Set up infrastructure for using OutputContent field

--- initialize_output.m
+++ initialize_output.m
@@ -1,5 +1,6 @@
 function out = initialize_output(options)
 
+%=== Initialize options which are always output
 out.EvolutionGenomeStats= cell(options.MaxIterations,options.Repetitions);
 out.EvolutionBestCost= zeros(options.MaxIterations,options.Repetitions);
 out.EvolutionBestCostTest= zeros(options.MaxIterations,options.Repetitions) ;
@@ -10,3 +11,16 @@ out.BestGenomePlot= cellfun(@(x) zeros(options.MaxIterations,options.NumFeatures
 out.IterationTime= zeros(1,options.Repetitions);
 out.CurrentIteration = 1;
 out.CurrentRepetition = 1;
+
+switch options.OutputContent
+    case 'detailed'
+        %=== Initialize additional out fields
+        
+        
+    case 'debug'
+        %=== Initialize many additional out fields for debug purposes
+        
+        
+    case 'normal'
+        %=== No additional out fields required
+end
