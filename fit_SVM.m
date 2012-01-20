3ba2683c4a04929892f237a2391067554b42e4a3
Alistair Johnson <alistairewj@gmail.com>
2012-01-20 12:55:36 +0000

    Fixed fit_SVM to properly use the bioinfo toolbox if libsvm is in the path. Also changed it to use SMO since quadratic program is worthless. Ehem, I mean, didn't converge...

--- fit_SVM.m
+++ fit_SVM.m
@@ -1,16 +1,22 @@
 function [ train_pred, test_pred  ] = ...
     fit_SVM(train_data,train_target,test_data,test_target)
 % FIT_SVM uses an SVM as the fitness function (wrapper)
+%   Requires the Bioinformatics toolbox
+
+%=== Create a function handle for the original svmtrain
+org_svmtrain = str2func([matlabroot '/toolbox/bioinfo/biolearning/svmtrain']);
 
 % train the model
-model = svmtrain(train_data,train_target);
+model = org_svmtrain(train_data,train_target,...
+    'Kernel_Function', 'rbf',...
+    'RBF_Sigma', 1,...
+    'Method', 'SMO');
 
 % Apply to your data
 [train_pred] = ...
     svmclassify(model, train_data);
 [test_pred] = ...
     svmclassify(model, test_data);
-
 
 end
   
