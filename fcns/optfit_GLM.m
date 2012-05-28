2cfc7d3ca7545c3148493c5b8e3ff871e060a2ef
Alistair Johnson <alistairewj@gmail.com>
2012-05-28 13:07:29 +0100

    Forgot to cmmit optfit_GLM - removed excess code that calculated a regression

--- fcns/optfit_GLM.m
+++ fcns/optfit_GLM.m
@@ -6,10 +6,5 @@ opt=struct('Distribution', 'normal', ...
         'link', 'identity', ...
         'constant', 'off',...
         'estdisp', 'off');
-    
-% train the multivariate linear regression using GLM
-b2=glmfit(train_data,train_target,'normal','link','identity','constant','off');
-train_pred=glmval(b2,train_data,'identity','constant','off');
-test_pred=glmval(b2,test_data,'identity','constant','off');
 
 end
